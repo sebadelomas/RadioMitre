2f78e8d2d9767d89b5a02755bb52212612ae061a
sebadelomas <sebadelomas@hotmail.com>
2019-10-03 21:04:36 -0300

    fix typo

--- src/components/Navigation/Footer/Footer.js
+++ src/components/Navigation/Footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.module.css';
 const footer = () => (
     <footer className={styles.Footer}>
         <p>Creado por Sebastián Cabrera</p>
-        <a style={{textDecoration: "none"}} target="_blank" rel="noopener noreferrer" href="https://github.com/sebadelomas/radioMitre"><p>Link al repo</p></a>
+        <a style={{textDecoration: "none"}} target="_blank" rel="noopener noreferrer" href="https://github.com/sebadelomas/RadioMitre"><p>Link al repo</p></a>
     </footer>
 );
 
